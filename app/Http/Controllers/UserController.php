3eaed15854b479e846d14bb314ed8dbf250d3fbc
sarunweerawadtanapong <emopalm.my@gmail.com>
2018-04-12 06:07:57 +0700

    *-*-

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
         //return $this->middleware('checkAge');
         return 'test >> '.$id;
 
+
         //return view('user.profile', ['user' => User::findOrFail($id)]);
     }
 }
